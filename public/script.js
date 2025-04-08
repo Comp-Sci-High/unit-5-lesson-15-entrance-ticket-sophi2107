5ce2f1865f3b372f88ab548981860fcfb7007db2
sophi2107 <sophia.rivera25@compscihigh.org>
2025-04-08 13:59:45 +0000

    uhhh

--- public/script.js
+++ public/script.js
@@ -1,4 +1,28 @@
 // Create a function that tests the post route handler [2 pts]
+async function newPotion(){
+try {
+    // fill in the path
+    const response = await fetch("/create-potion", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+           label:"pearlywhite",
+           effect: "Drowsiness",
+           ingredients: "Vampires Blood",
+    color: "Red",
+    isExplosive: false,
+        })
+
+    });
+    const json = await response.json();
+    console.log(json);
+} catch (error) {
+    console.error(error.message);
+}
+}
+newPotion()
 
 
 
